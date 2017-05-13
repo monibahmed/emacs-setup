527a514c16d48efcf0a8bfd3d11c878a6bf6350d
Monib Ahmed <monibahmed@gmail.com>
2017-05-12 18:53:09 -0700

    Updated YAsnippets

--- init.el
+++ init.el
@@ -13,10 +13,26 @@
 (require 'ein-loaddefs)
 (require 'ein-notebook)
 (require 'ein-subpackages)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(require 'impatient-mode)
+(add-to-list 'load-path "~/.emacs.d/impatient-mode")
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/plugins/snippets"          ;; personal snippets
+        "~/.emacs.d/plugins/yasmate/snippets" ;; the yasmate collection
+        ))
+(yas-global-mode 1)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
-
 
 (defvar myPackages
   '(better-defaults
@@ -35,7 +51,8 @@
 ;; --------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
+;;(load-theme 'material t) ;; load material theme
+(load-theme 'atom-dark t)
 (global-linum-mode t) ;; enable line numbers globally
 (setq-default indent-tabs-mode nil)
 
@@ -70,49 +87,17 @@
        nil "_"))))
 
 
-;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (leuven)))
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default)))
- '(fci-rule-color "#ECEFF1")
- '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages (quote (material-theme better-defaults)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#B71C1C")
-     (40 . "#FF5722")
-     (60 . "#FFA000")
-     (80 . "#558b2f")
-     (100 . "#00796b")
-     (120 . "#2196f3")
-     (140 . "#4527A0")
-     (160 . "#B71C1C")
-     (180 . "#FF5722")
-     (200 . "#FFA000")
-     (220 . "#558b2f")
-     (240 . "#00796b")
-     (260 . "#2196f3")
-     (280 . "#4527A0")
-     (300 . "#B71C1C")
-     (320 . "#FF5722")
-     (340 . "#FFA000")
-     (360 . "#558b2f"))))
- '(vc-annotate-very-old-color nil))
+    (yasnippet htmlize simple-httpd web-mode py-autopep8 material-theme impatient-mode flycheck elpy ein better-defaults atom-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
